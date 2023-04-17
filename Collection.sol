7ebb34e51e729f266a75060fd554ea282754af0a
cryptycmaster <127647797+cryptycmaster@users.noreply.github.com>
2023-04-17 18:17:05 +0800

    Update Collection.sol

--- Collection.sol
+++ Collection.sol
@@ -1,26 +1,5 @@
 // SPDX-License-Identifier: MIT LICENSE
 
-/*
-N2D NFT ERC721 NFT Smart Contract.
-
-Follow/Subscribe Youtube, Github, IM, Tiktok
-for more amazing content!!
-@Net2Dev
-███╗░░██╗███████╗████████╗██████╗░██████╗░███████╗██╗░░░██╗
-████╗░██║██╔════╝╚══██╔══╝╚════██╗██╔══██╗██╔════╝██║░░░██║
-██╔██╗██║█████╗░░░░░██║░░░░░███╔═╝██║░░██║█████╗░░╚██╗░██╔╝
-██║╚████║██╔══╝░░░░░██║░░░██╔══╝░░██║░░██║██╔══╝░░░╚████╔╝░
-██║░╚███║███████╗░░░██║░░░███████╗██████╔╝███████╗░░╚██╔╝░░
-╚═╝░░╚══╝╚══════╝░░░╚═╝░░░╚══════╝╚═════╝░╚══════╝░░░╚═╝░░░
-THIS CONTRACT IS AVAILABLE FOR EDUCATIONAL 
-PURPOSES ONLY. YOU ARE SOLELY REPONSIBLE 
-FOR ITS USE. I AM NOT RESPONSIBLE FOR ANY
-OTHER USE. THIS IS TRAINING/EDUCATIONAL
-MATERIAL. ONLY USE IT IF YOU AGREE TO THE
-TERMS SPECIFIED ABOVE.
-*/
-
-
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
